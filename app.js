85941399e50d63d4497e6cd9486b77057a9d671c
Yves Desjardins <yvesdesjardins@gmail.com>
2020-02-13 13:57:12 -0800

    complete basic implementation of bot and tests

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 require('dotenv').config();
 
 const Snoowrap = require('snoowrap');
-const Snoostorm = require('snoostorm');
+const { InboxStream, CommentStream, SubmissionStream } = require('snoostorm');
+
+// Build Snoowrap and Snoostorm clients
+const r = new Snoowrap({
+  userAgent: 'readdit-bot',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
+});
+
+// Configure options for stream: subreddit & results per query
+const streamOpts = {
+  subreddit: 'all',
+  results: 25
+};
+
+// Create a Snoostorm CommentStream with the specified options
+const comments = new CommentStream(r, streamOpts);
+
+console.log("Connected");
+
+// On comment, perform whatever logic you want to do
+comments.on('comment', console.log);
--- test/test-app.js
+++ test/test-app.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 var expect  = require('chai').expect;
+const Snoowrap = require('snoowrap');
 
-it('Always pass for now', function(done) {
-  expect(true).to.equal(true);
-  done();
+const r = new Snoowrap({
+  userAgent: 'readdit-bot',
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
+});
+
+it('See if sucessfully connected to reddit', function(done) {
+  r.getHot().map(post => post.title).then(results => {
+    expect(results.length).to.be.at.least(1);
+    done();
+  });
 });
