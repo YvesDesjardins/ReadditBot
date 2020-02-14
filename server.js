61465287c2746c0c1d77ea3ff04834036feca020
Yves Desjardins <yvesdesjardins@gmail.com>
2020-02-14 11:35:35 -0800

    basic setup of websockets

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const snoo = require('./src/snooStuff.js');
 const PORT = 3000; //hard-coded for simplicity
+const WebSocket = require('ws');
 
 const app = express();
+const wss = new WebSocket.Server({ server: app })
 
 app.get('/', function (req, res) {
   res.send(snoo.latestComment());
@@ -11,8 +13,19 @@ app.get('/', function (req, res) {
 app.get('/all', function (req, res) {
   res.send(snoo.allComments());
 });
+app.get('/client', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
 
+wss.on('connection', (ws) => {
+  ws.send('hello world!');
+  // ws.on('message', (data) => {
+  // });
+
+  // ws.on('close', () => {
+  // });
+});
